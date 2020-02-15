1b16a922f2becde4e4291c7fdfd8ca7316eb5eaf
jwillits@illinois.edu <jwillits@illinois.edu>
2020-02-15 15:55:15 -0600

    cleaned up weight display code

--- Polyomino_World.py
+++ Polyomino_World.py
@@ -7,13 +7,14 @@ import numpy as np
 def main():
     np.set_printoptions(precision=3, suppress=True)
     dataset_filename = 'w8-8_s9_c8_0_10_0.csv'
-    model = 'WS_FV_2020_2_9_12_8_30'
+    model1 = 'WS_FV_2020_2_9_12_8_30'
+    model2 = 'WS_WS_2020_2_9_10_44_25'
     included_features = [1, 1, 1, 0]
     processor = 'CPU'
 
     the_dataset = dataset.DataSet(dataset_filename, None, included_features, processor)
     the_network = network.MlNet()
-    the_network.load_model(model, included_features, processor)
+    the_network.load_model(model2, included_features, processor)
 
     the_display = display.Display(the_dataset, the_network)
     the_display.root.mainloop()
